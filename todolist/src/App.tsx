289b0beee8a035ccd2b1c34779bea3fb721a6851
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-17 22:44:14 +0800

    Implement strike-through.

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,20 +1,39 @@
+import { useState } from "react";
 import ToDoList from "./components/ToDoList";
 
 export interface ToDo {
   id: number;
   description: string;
+  isDone: boolean;
   // due: Date;
 }
 
 let data = [
-  { id: 1, description: "Wash dishes at pumunta sa palengke putaingina" },
-  { id: 2, description: "Go to gym." },
+  {
+    id: 1,
+    description: "Wash dishes at pumunta sa palengke putaingina",
+    isDone: false,
+  },
+  { id: 2, description: "Go to gym.", isDone: false },
 ];
 
 const App = () => {
+  const [toDos, setToDos] = useState(data);
+
   return (
     <>
-      <ToDoList toDoList={data} />
+      <ToDoList
+        toDoList={toDos}
+        onDelete={(deletedToDo) =>
+          setToDos(
+            toDos.map((toDo) =>
+              toDo.description === deletedToDo.description
+                ? { ...toDo, isDone: !toDo.isDone }
+                : toDo
+            )
+          )
+        }
+      />
     </>
   );
 };
--- todolist/src/components/ToDoList.tsx
+++ todolist/src/components/ToDoList.tsx
@@ -1,11 +1,13 @@
+import { useEffect, useState } from "react";
 import { ToDo } from "../App";
 import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
 
 interface Props {
   toDoList: ToDo[];
+  onDelete: (deletedToDo: ToDo) => void;
 }
 
-const ToDoList = ({ toDoList }: Props) => {
+const ToDoList = ({ toDoList, onDelete }: Props) => {
   return (
     <>
       <ul className="list-group m-3">
@@ -13,8 +15,21 @@ const ToDoList = ({ toDoList }: Props) => {
           <li key={toDo.id} className="list-group-item">
             <div className="d-flex justify-content-between align-items-center">
               <div className="d-flex justify-content-between align-items-center">
-                <input type="checkbox" className="me-2" />
-                {toDo.description}
+                <input
+                  type="checkbox"
+                  className="me-2"
+                  onClick={() => onDelete(toDo)}
+                />
+
+                <div
+                  className={
+                    toDo.isDone === true
+                      ? "text-decoration-line-through"
+                      : "text-decoration-none"
+                  }
+                >
+                  {toDo.description}
+                </div>
               </div>
               <div className="d-flex ps-5 align-items-center">
                 <div className="me-1">
