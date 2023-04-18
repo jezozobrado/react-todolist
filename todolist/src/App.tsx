f9052f7165de7d1b2437db1b23cee72db02a33c5
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-18 21:40:35 +0800

    Add edit and cancel button.

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -3,17 +3,38 @@ import ToDoList from "./components/ToDoList";
 
 export interface ToDo {
   id: number;
+  title: string;
   description: string;
   isDone: boolean;
+  isEdited: boolean;
+  isHovered: boolean;
 }
 
 let data = [
   {
     id: 1,
+    title: "wash",
     description: "Wash dishes at pumunta sa palengke putaingina",
     isDone: false,
+    isEdited: false,
+    isHovered: false,
   },
-  { id: 2, description: "Go to gym.", isDone: false },
+  {
+    id: 2,
+    title: "gym",
+    description: "Go to gym.",
+    isDone: false,
+    isEdited: false,
+    isHovered: false,
+  },
+  {
+    id: 3,
+    title: "code",
+    description: "Django, React JS, Welcome to New york.",
+    isDone: false,
+    isEdited: false,
+    isHovered: false,
+  },
 ];
 
 const App = () => {
@@ -33,12 +54,36 @@ const App = () => {
       toDos.filter((toDo) => toDo.description !== deletedToDo.description)
     );
 
+  const handleEdit = (editedToDo: ToDo) => {
+    setToDos(
+      toDos.map((toDo) =>
+        toDo.description === editedToDo.description
+          ? { ...toDo, isEdited: !toDo.isEdited }
+          : toDo
+      )
+    );
+  };
+
+  const handleHover = (hoveredToDo: ToDo) => {
+    console.log("Hovered!");
+
+    setToDos(
+      toDos.map((toDo) =>
+        toDo.description === hoveredToDo.description
+          ? { ...toDo, isHovered: !toDo.isHovered }
+          : toDo
+      )
+    );
+  };
+
   return (
     <>
       <ToDoList
         toDoList={toDos}
         onStrike={handleStrike}
         onDelete={handleDelete}
+        onHover={handleHover}
+        onEdit={handleEdit}
       />
     </>
   );
--- todolist/src/components/ToDoList.tsx
+++ todolist/src/components/ToDoList.tsx
@@ -1,59 +1,84 @@
-import { useEffect, useState } from "react";
 import { ToDo } from "../App";
 import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+import { VscSaveAs } from "react-icons/vsc";
+import { MdOutlineCancel } from "react-icons/md";
 
 interface Props {
   toDoList: ToDo[];
   onStrike: (strikedToDo: ToDo) => void;
   onDelete: (deletedToDo: ToDo) => void;
+  onHover: (hoveredToDo: ToDo) => void;
+  onEdit: (editedToDo: ToDo) => void;
 }
 
-const ToDoList = ({ toDoList, onStrike, onDelete }: Props) => {
-  //   console.log(toDoList[0].isDeleted);
-
+const ToDoList = ({ toDoList, onStrike, onDelete, onHover, onEdit }: Props) => {
   return (
     <>
-      <ul className="list-group m-3">
+      <ul className="list-group m-3 todolist__container">
         {toDoList.map((toDo) => (
-          <li key={toDo.id} className="list-group-item">
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="d-flex justify-content-between align-items-center">
-                <input
-                  type="checkbox"
-                  className="me-2"
-                  onClick={() => onStrike(toDo)}
-                />
+          <div key={toDo.id}>
+            <li className="list-group-item py-1 mb-2 ">
+              <div
+                className="d-flex justify-content-between"
+                onMouseEnter={() => onHover(toDo)}
+                onMouseLeave={() => onHover(toDo)}
+              >
+                <div className="d-flex justify-content-between align-items-baseline">
+                  <input
+                    type="checkbox"
+                    className="me-2"
+                    onClick={() => onStrike(toDo)}
+                  />
+                  {toDo.isEdited ? (
+                    <div className="d-flex flex-column flex-grow-1">
+                      <input
+                        type="text"
+                        value={toDo.title}
+                        className="border-0"
+                      />
+                      <input
+                        type="text"
+                        value={toDo.description}
+                        className="border-0"
+                      />
+                    </div>
+                  ) : (
+                    <div
+                      className={
+                        toDo.isDone
+                          ? "text-decoration-line-through"
+                          : "text-decoration-none"
+                      }
+                    >
+                      <div className="fw-bold">{toDo.title}</div>
+                      {toDo.description}
+                    </div>
+                  )}
+                </div>
+                {toDo.isHovered ? (
+                  <div className="d-flex p-0 align-items-start">
+                    <button
+                      className="btn border-0 py-0 px-0"
+                      onClick={() => onEdit(toDo)}
+                    >
+                      {toDo.isEdited ? <VscSaveAs /> : <AiOutlineEdit />}
+                    </button>
 
-                <div
-                  className={
-                    toDo.isDone === true
-                      ? "text-decoration-line-through"
-                      : "text-decoration-none"
-                  }
-                >
-                  {toDo.description}
-                </div>
+                    <button
+                      className="btn border-0 py-0"
+                      onClick={() => onDelete(toDo)}
+                    >
+                      {toDo.isEdited ? (
+                        <MdOutlineCancel color="red" />
+                      ) : (
+                        <AiOutlineDelete color="red" />
+                      )}
+                    </button>
+                  </div>
+                ) : null}
               </div>
-              <div className="d-flex ps-5 align-items-center">
-                <div className="me-1">
-                  <button
-                    className="btn border-0"
-                    onClick={() => console.log("Edited.")}
-                  >
-                    <AiOutlineEdit />
-                  </button>
-                </div>
-                <div className="">
-                  <button
-                    className="btn border-0"
-                    onClick={() => onDelete(toDo)}
-                  >
-                    <AiOutlineDelete color="red" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </li>
+            </li>
+          </div>
         ))}
       </ul>
     </>
