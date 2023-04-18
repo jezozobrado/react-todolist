39e1a9afee4280f1945f548e2636c22c6033503c
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-19 00:46:01 +0800

    Reimplement strike through.

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -89,7 +89,7 @@ const App = () => {
 
   return (
     <>
-      <ToDoList toDos={toDos} onDelete={handleDelete} />
+      <ToDoList toDos={toDos} onDelete={handleDelete} onStrike={handleStrike} />
     </>
   );
 };
--- todolist/src/components/ToDoList.tsx
+++ todolist/src/components/ToDoList.tsx
@@ -6,21 +6,30 @@ import { MdOutlineCancel } from "react-icons/md";
 interface Props {
   toDos: ToDo[];
   onDelete: (deletedToDo: ToDo) => void;
+  onStrike: (StrikedToDo: ToDo) => void;
 }
 
-const ToDoList = ({ toDos, onDelete }: Props) => {
+const ToDoList = ({ toDos, onDelete, onStrike }: Props) => {
   console.table(toDos);
   return (
     <section>
       {toDos.map((toDo) => (
         <div className="todo__container mb-3">
           <div className="ms-2 mt-1">
-            <input type="checkbox"></input>
+            <input type="checkbox" onClick={() => onStrike(toDo)}></input>
           </div>
 
           <div className="d-flex flex-column my-0">
-            <p className="mb-0">{toDo.title}</p>
-            <p className="fw-light fs-4">{toDo.description}</p>
+            <div
+              className={
+                toDo.isDone
+                  ? "text-decoration-line-through"
+                  : "text-decoration-none"
+              }
+            >
+              <p className="mb-0">{toDo.title}</p>
+              <p className="fw-light fs-4">{toDo.description}</p>
+            </div>
           </div>
 
           <div className="">
